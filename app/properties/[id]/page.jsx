1591b8de4a8d469112c7d16276f43e39fcdf5165
Guillermo Eguizabal <cgeguizabal@gmail.com>
2025-05-23 16:39:41 -0600

    BookMark and Share

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,14 +1,16 @@
 export const dynamic = "force-dynamic";
 
-import PropertyImages from "@/components/PropertyImages";
-import connectDB from "@/config/database";
-import PropertyDetails from "@/components/PropertiesDetails";
-import Property from "@/models/Property";
 import PropertyHeaderImage from "@/components/PropertyHeaderImage";
+import PropertyDetails from "@/components/PropertiesDetails";
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+import PropertyImages from "@/components/PropertyImages";
+import BookmarkButton from "@/components/BookmarkButton";
+import ShareButtons from "@/components/ShareButtons";
+import PropertyContactForm from "@/components/PropertyContactForm";
+import { convertToSerializeableObject } from "@/utils/convertToObject";
 import Link from "next/link";
 import { FaArrowLeft } from "react-icons/fa";
-
-import { convertToSerializeableObject } from "@/utils/convertToObject";
 
 const PropertyPage = async ({ params }) => {
   const resolvedParams = await params; // <-- Await params here
@@ -43,6 +45,12 @@ const PropertyPage = async ({ params }) => {
         <div className="container m-auto py-10 px-6">
           <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
             <PropertyDetails property={property} />
+            {/* <!-- Sidebar --> */}
+            <aside className="space-y-4">
+              <BookmarkButton property={property} />
+              <ShareButtons property={property} />
+              <PropertyContactForm property={property} />
+            </aside>
           </div>
         </div>
       </section>
