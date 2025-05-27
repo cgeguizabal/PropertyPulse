1a8deb4761b070d5ace680381465bcc7ce5aa747
Guillermo Eguizabal <cgeguizabal@gmail.com>
2025-05-26 18:28:28 -0600

    bug, mobile log in button fixed

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -131,7 +131,7 @@ function Navbar() {
                 <Link href="/messages" className="relative group">
                   <button
                     type="button"
-                    className="relative rounded-full cursor-pointer bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                    className="relative rounded-full md:cursor-pointer bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                   >
                     <span className="absolute -inset-1.5"></span>
                     <span className="sr-only">View notifications</span>
