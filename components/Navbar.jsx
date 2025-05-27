482b016693a0d1e6e335738378b492d7ebf5e864
Guillermo Eguizabal <cgeguizabal@gmail.com>
2025-05-26 18:43:04 -0600

    mobile bug fixed

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -106,7 +106,7 @@ function Navbar() {
               </div>
             </div>
 
-            {/* <!-- Right Side Menu (Logged Out) --> */}
+            {/* <!-- Right Side Menu (Logged in) --> */}
             {!session && (
               <div className="hidden md:block md:ml-6">
                 <div className="flex items-center">
@@ -125,7 +125,7 @@ function Navbar() {
               </div>
             )}
 
-            {/* <!-- Right Side Menu (Logged In) --> */}
+            {/* <!-- Right Side Menu (Logged out) --> */}
             {session && (
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0">
                 <Link href="/messages" className="relative group">
@@ -255,12 +255,18 @@ function Navbar() {
                   Add Property
                 </Link>
               )}
-              {!session && (
-                <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-5">
-                  <i className="fa-brands fa-google mr-2"></i>
-                  <span>Login or Register</span>
-                </button>
-              )}
+              {!session &&
+                providers &&
+                Object.values(providers).map((provider, index) => (
+                  <button
+                    key={index}
+                    onClick={() => signIn(provider.id)}
+                    className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-5 w-full"
+                  >
+                    <FaGoogle className="mr-2" />
+                    <span>Login or Register</span>
+                  </button>
+                ))}
             </div>
           </div>
         )}
